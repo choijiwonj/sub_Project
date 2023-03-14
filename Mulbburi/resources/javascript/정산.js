399e045287ff8ca4cd67b10e828ba56b15f75efe
P.minkyoung <tofhddl94@gmail.com>
2023-03-14 16:37:26 +0900

    COMMIT

---
+++ Mulbburi/resources/javascript/정산.js
@@ -0,0 +1,20 @@
+function click(){
+    const $t1 = document.querySelector(".t1");
+    const $t2 = document.querySelector(".t2");
+    const $background1 = document.querySelector(".background1");
+    const $background2 = document.querySelector(".background2");
+
+    $t1.addEventListener("click", () => {
+        $background1.style.display = "flex";
+        $background2.style.display = "none";
+        console.log("t1");
+    });
+
+    $t2.addEventListener("click", () => {
+        $background2.style.display = "flex";
+        $background1.style.display = "none";
+        console.log("t2");
+    })
+}
+
+window.onload = click;
